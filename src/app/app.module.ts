02cc90568a84de8b05968d6a39bab7f13eb27872
Flavio Rico <flavio.rico@fygdevelopers.com>
2020-07-20 21:03:51 -0500

    Incorporación inicial del esqueleto para Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderLoginComponent } from './header-login/header-login.component';
+import { DataLoginComponent } from './data-login/data-login.component';
+import { FooterLoginComponent } from './footer-login/footer-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderLoginComponent,
+    DataLoginComponent,
+    FooterLoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data-login/data-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-login',
+  templateUrl: './data-login.component.html',
+  styleUrls: ['./data-login.component.css']
+})
+export class DataLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
