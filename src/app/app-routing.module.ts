1b3401acae972988ef705b85624a490a5fcd535a
Flavio Rico <flavio.rico@fygdevelopers.com>
2020-07-22 00:08:54 -0500

    Incorporación de ruta para la interacción con el componente posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProfileComponent } from './profile/profile.component';
+import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
   },
   {
     path: '',
