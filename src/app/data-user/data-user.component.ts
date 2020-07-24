21530df47e56e35c3d2afcf5a0983e05a3a91ba7
Flavio Rico <flavio.rico@fygdevelopers.com>
2020-07-23 21:38:16 -0500

    Creación de componente para representar el data del usuario

---
+++ src/app/data-user/data-user.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service';
+import { ActivatedRoute, Params} from '@angular/router';
+
+@Component({
+  selector: 'app-data-user',
+  templateUrl: './data-user.component.html',
+  styleUrls: ['./data-user.component.css']
+})
+export class DataUserComponent implements OnInit {
+  user;
+  id: number;
+
+  constructor(
+    private loginService: LoginService,
+    private route: ActivatedRoute
+    ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params.id;
+    }
+    );
+    this.fetchUser();
+  }
+  fetchUser(){
+    this.loginService.getDataUser(this.id).subscribe(user => {
+      this.user = user;
+      console.log(this.user);
+    });
+  }
+}
